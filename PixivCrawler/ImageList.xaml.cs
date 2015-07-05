537d3429188fc36d8467bc72163ddc1165ed279c
SNTruer <tkdlek5425@naver.com>
2015-07-05 21:14:16 +0900

    first commit

---
+++ PixivCrawler/ImageList.xaml.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Security;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Net;
+using System.Net.Security;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.Threading;
+using System.IO;
+using System.Web;
+using mshtml;
+
+namespace PixivCrawler
+{
+    /// <summary>
+    /// ImageList.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class ImageList : Window
+    {
+        Image[,] SearchedItem = new Image[4, 5];
+        WebBrowser PixivBrowser;
+        String URL, Tag;
+        CookieContainer cookie;
+        int Count = 0;
+        int PageNumber = 1;
+        public ImageList(WebBrowser PixivBrowser,String Tag,CookieContainer cookie)
+        {
+            InitializeComponent();
+            this.PixivBrowser = PixivBrowser;
+            this.Tag = Tag;
+            this.cookie = cookie;
+            for(int i=0;i<4;i++)
+            {
+                for(int j=0;j<5;j++)
+                {
+                    SearchedItem[i, j] = new Image();
+                    /*var Img = new BitmapImage();
+                    Img.BeginInit();
+                    Img.UriSource = new Uri(("C:/Users/CATDOG/Pictures/5.jpg"), UriKind.RelativeOrAbsolute);
+                    Img.CacheOption = BitmapCacheOption.OnLoad;
+                    Img.EndInit();*/
+                    //SearchedItem[i, j].Source = Img;
+                    SearchedItem[i, j].Width = 150;
+                    SearchedItem[i, j].Height = 150;
+                    ImageGrid.Children.Add(SearchedItem[i, j]);
+                    Grid.SetRow(SearchedItem[i, j], i);
+                    Grid.SetColumn(SearchedItem[i, j], j);
+                }
+            }
+            Get_SearchList(1);
+            for(int i=0;i<4;i++)
+            {
+                for(int j=0;j<5;j++)
+                {
+                    /*ImageGrid.Children.Add(SearchedItem[i, j]);
+                    Grid.SetRow(SearchedItem[i, j], i);
+                    Grid.SetColumn(SearchedItem[i, j], j);*/
+                }
+            }
+        }
+
+        private void __Get_Image(int Count)
+        {
+            var Doc = PixivBrowser.Document as HTMLDocument;
+            var ElementCollection = Doc.getElementsByTagName("img");
+            foreach(IHTMLElement Element in ElementCollection)
+            {
+                if(Element.parentElement.className=="_layout-thumbnail ui-modal-trigger")
+                {
+                    var Img = new BitmapImage();
+                    Img.BeginInit();
+                    Img.UriSource = new Uri(Element.getAttribute("src"),UriKind.RelativeOrAbsolute);
+                    Img.CacheOption = BitmapCacheOption.OnLoad;
+                    Img.EndInit();
+                    SearchedItem[Count / 5, Count % 5].Source = Img;
+                }
+            }
+        }
+
+        private void _Get_Image(object O)
+        {
+            String URL = ((Tuple<String, int>)O).Item1;
+            int count = ((Tuple<String, int>)O).Item2;
+            try
+            {
+                HttpWebRequest myRequest = (HttpWebRequest)WebRequest.Create("http://www.pixiv.net" + URL);
+                string session = cookie.GetCookies(new Uri("http://www.pixiv.net"))["PHPSESSID"].Value;
+                //Cookie C = new Cookie("PHPSESSID", session, "/", ".pixiv.net");
+                myRequest.CookieContainer = cookie;
+                //myRequest.Method = "POST";
+                //myRequest.UserAgent = "Mozilla/5.0 (Windows; U; Windows NT 6.0; zh-CN; rv:1.9.0.6) Gecko/2009011913 Firefox/3.0.6";
+                //myRequest.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
+                /*myRequest.Headers.Add("Accept-Language", "zh-cn,zh;q=0.7,ja;q=0.3");
+                myRequest.Headers.Add("Accept-Encoding", "gzip,deflate");
+                myRequest.Headers.Add("Accept-Charset", "gb18030,utf-8;q=0.7,*;q=0.7");
+                myRequest.Referer = "http://www.pixiv.net/index.php";*/
+                myRequest.Referer = "http://www.pixiv.net/index.php";
+
+                HttpWebResponse myResponse = (HttpWebResponse)myRequest.GetResponse();
+
+                StreamReader reader = new StreamReader(myResponse.GetResponseStream(), Encoding.UTF8);
+
+                String content = reader.ReadToEnd();
+
+                int S_value = content.IndexOf("<div class=\"wrapper\"><span class=\"close ui-modal-close\"><i class=\"_icon-12 size-2x _icon-close\">");
+
+                int StIdx = content.IndexOf("data-src=\"", S_value);
+                int EdIdx = content.IndexOf("\"", StIdx + 10);
+
+                String Src = content.Substring(StIdx + 10, EdIdx - StIdx - 10);
+                foreach (Cookie Cook in myResponse.Cookies)
+                {
+                    myRequest.CookieContainer.Add(Cook);
+                }
+
+                myRequest = (HttpWebRequest)WebRequest.Create(Src);
+                myRequest.CookieContainer = cookie;
+                //myRequest.Referer = "http://www.pixiv.net/member_illust.php?mode=medium&illust_id=50284907";
+                //myRequest.UserAgent = "Mozilla/5.0 (Windows; U; Windows NT 6.0; zh-CN; rv:1.9.0.6) Gecko/2009011913 Firefox/3.0.6";
+                //myRequest.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
+                //myRequest.Headers.Add("Accept-Encoding", "gzip,deflate");
+                //myRequest.Headers.Add("Accept-Charset", "gb18030,utf-8;q=0.7,*;q=0.7");
+                myRequest.Referer = "http://www.pixiv.net/index.php";
+                myResponse = (HttpWebResponse)myRequest.GetResponse();
+                using (var ImageS = myResponse.GetResponseStream())
+                {
+                    byte[] Buffer = new byte[32768];
+                    int read = 0;
+
+
+                    int chunk;
+                    while ((chunk = ImageS.Read(Buffer, read, Buffer.Length - read)) > 0)
+                    {
+                        read += chunk;
+
+
+                        // If we've reached the end of our buffer, check to see if there's
+                        // any more information
+                        if (read == Buffer.Length)
+                        {
+                            int nextByte = ImageS.ReadByte();
+
+
+                            // End of stream? If so, we're done
+                            if (nextByte == -1)
+                            {
+                                break;
+                            }
+
+
+                            // Nope. Resize the buffer, put in the byte we've just
+                            // read, and continue
+                            byte[] newBuffer = new byte[Buffer.Length * 2];
+                            Array.Copy(Buffer, newBuffer, Buffer.Length);
+                            newBuffer[read] = (byte)nextByte;
+                            Buffer = newBuffer;
+                            read++;
+                        }
+                    }
+                    Dispatcher.Invoke(DispatcherPriority.Normal, new Action(delegate
+                    {
+                        using (MemoryStream ImageMemoryS = new MemoryStream(Buffer))
+                        {
+                            var Img = new BitmapImage();
+                            //FileStream FileS = new FileStream("1.jpg", FileMode.Create);
+                            //FileS.Write(Buffer, 0, Buffer.Length);
+                            //Img.UriSource = new Uri((Src), UriKind.RelativeOrAbsolute);
+                            Img.BeginInit();
+                            Img.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+                            Img.CacheOption = BitmapCacheOption.OnLoad;
+                            Img.UriSource = null;
+                            Img.StreamSource = ImageMemoryS;
+                            Img.EndInit();
+                            if(count<20) SearchedItem[count / 5, count % 5].Source = Img;
+
+                            //this.Dispatcher.Invoke(
+                            //(ThreadStart)(() => { }), DispatcherPriority.ApplicationIdle);
+                        }
+                    }));
+                }
+
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.ToString());
+                //MessageBox.Show(ex.Status.ToString());
+            }
+        }
+
+        private void Get_SearchList(int PageNumber)
+        {
+            Count = 0;
+            URL = "http://www.pixiv.net/search.php?word=" + Tag + "&order=date_d&p=" + PageNumber.ToString();
+            //URL = "http://www.pixiv.net/member_illust.php?mode=medium&illust_id=50311630";
+            //URL = "http://www.pixiv.net/member_illust.php?mode=medium&illust_id=50311108";
+            //URL = "http://i3.pixiv.net/img-original/img/2015/05/10/16/15/31/50306658_p0.jpg";
+            //URL = "http://i1.pixiv.net/img-original/img/2015/05/08/00/00/01/50259632_p0.jpg";
+            //PixivBrowser.LoadCompleted += _Get_SearchList;
+
+            HttpWebRequest myRequest = (HttpWebRequest)WebRequest.Create(URL);
+            myRequest.CookieContainer = cookie;
+            myRequest.Referer = "http://www.pixiv.net/index.php";
+            HttpWebResponse myResponse = (HttpWebResponse)myRequest.GetResponse();
+            StreamReader reader = new StreamReader(myResponse.GetResponseStream(), Encoding.UTF8);
+            String content = reader.ReadToEnd();
+
+            int StIdx = content.IndexOf("<li class=\"image-item \">");
+            int EdIdx;
+            while(StIdx>0)
+            {
+                StIdx = content.IndexOf("<a href=\"",StIdx);
+                if (StIdx < 0) break;
+                StIdx = content.IndexOf("\"", StIdx);
+                EdIdx = content.IndexOf("\"", StIdx + 1);
+                StIdx++;
+                String SubURL = content.Substring(StIdx,EdIdx-StIdx);
+                Thread T = new Thread(new ParameterizedThreadStart(_Get_Image));
+                T.Start(Tuple.Create(SubURL, Count));
+                Count++;
+                StIdx = content.IndexOf("<li class=\"image-item \">",StIdx+1);
+            }
+        }
+
+        private void _Get_SearchList(object sender,NavigationEventArgs e)
+        {
+            var Doc = PixivBrowser.Document as HTMLDocument;
+            var ElementCollection = Doc.getElementsByTagName("li");
+            PixivBrowser.LoadCompleted -= _Get_SearchList;
+            foreach (IHTMLElement Element in ElementCollection)
+            {
+                //var ChildrenCollection = Element.children as HTMLElementCollection;
+
+                if (Element.className != "image-item ") continue;
+                dynamic ChildrenCollection = Element.children;
+
+                foreach (IHTMLElement ChildrenElement in ChildrenCollection)
+                {
+                    if (ChildrenElement.className == "work  _work ")
+                    {
+                        //PixivBrowser.Navigate(ChildrenElement.getAttribute("href"));
+                        Thread T = new Thread(new ParameterizedThreadStart(Get_Image));
+                        T.Start(ChildrenElement.getAttribute("href"));
+                        break;
+                    }
+                }
+                break;
+            }
+        }
+
+        private void Get_Image(object O)
+        {
+            String To_Image_Url = (String)O;
+
+            Dispatcher.Invoke(DispatcherPriority.Normal, new Action(delegate
+            {
+                WebBrowser P = PixivBrowser;
+                //P.LoadCompleted += _Get_Image;
+                P.Navigate(To_Image_Url);
+            }));
+            Thread.Sleep(5000);
+        }
+    }
+}
---
+++ PixivCrawler/MainWindow.xaml.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Security;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Net;
+using System.Net.Security;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.Threading;
+using System.IO;
+using System.Web;
+using mshtml;
+
+namespace PixivCrawler
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        WebBrowser PixivBrowser2 = new WebBrowser();
+        CookieContainer cookie = new CookieContainer();
+        //WebClient PixivBrowser = new WebClient();
+
+        public MainWindow()
+        {
+            deletecookie();
+            InitializeComponent();
+            //PixivBrowser = new WebBrowser();
+            ServicePointManager.ServerCertificateValidationCallback = new RemoteCertificateValidationCallback(ValidateServerCertificate);
+            Login.IsEnabled = false;
+            //PixivBrowser.Visibility = System.Windows.Visibility.Visible;
+            PixivBrowser.LoadCompleted += Login_Method;
+            PixivBrowser.Navigate("https://www.secure.pixiv.net/login.php");
+            Uri PixivLogin = new Uri("https://www.secure.pixiv.net/login.php");
+            SearchButton.IsEnabled = false;
+        }
+
+        private void deletecookie()
+        {
+            string[] theCookies = System.IO.Directory.GetFiles(Environment.GetFolderPath(Environment.SpecialFolder.Cookies));
+
+            foreach (string currentFile in theCookies)
+            {
+                try
+                {
+                    System.IO.File.Delete(currentFile);
+                }
+                catch (Exception ex)
+                {
+                    //MessageBox.Show(ex.Message);
+                }
+            }
+        }
+        private void TextBoxPreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !IsTextAllowed(sender as TextBox, e.Text);
+        }
+
+        private static bool IsTextAllowed(TextBox textBox, string text)
+        {
+            System.Text.RegularExpressions.Regex regex = new System.Text.RegularExpressions.Regex("[^0-9.-]+"); //regex that matches disallowed text
+            return !regex.IsMatch(text);
+        }
+
+        private void Login_Method(object sender, NavigationEventArgs e)
+        {
+            Login.IsEnabled = true;
+        }
+
+        public static bool ValidateServerCertificate(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        {
+            return false;
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            String ID = IDTextBox.Text;
+            String PW = PasswordTextBox.Password;
+
+            var Doc = PixivBrowser.Document as HTMLDocument;
+
+            var LoginElementCollection = Doc.getElementsByTagName("input");
+
+
+            foreach (IHTMLElement LoginElement in LoginElementCollection)
+            {
+                int i = 0;
+                if (LoginElement.id == "login_pixiv_id")
+                {
+                    LoginElement.setAttribute("value", ID);
+                }
+                else if(LoginElement.id=="login_password")
+                {
+                    LoginElement.setAttribute("value", PW);
+                    //break;
+                }
+                else if(LoginElement.getAttribute("name")=="skip")
+                {
+                    //LoginElement.setAttribute("checked", "unchecked");
+                    LoginElement.click();
+                    //MessageBox.Show(LoginElement.outerHTML);
+                }
+            }
+            LoginElementCollection = Doc.getElementsByTagName("button");
+            foreach (IHTMLElement LoginElement in LoginElementCollection)
+            {
+                if (LoginElement.id == "login_submit") LoginElement.click();
+            }
+            PixivBrowser.LoadCompleted += CheckLogin;
+        }
+
+        private void CheckLogin(object sender,NavigationEventArgs e)
+        {
+            if (PixivBrowser.Source == new Uri("https://www.secure.pixiv.net/login.php")) MessageBox.Show("Failed!");
+            else
+            {
+                Login.IsEnabled = false;
+                SearchButton.IsEnabled = true;
+                PixivBrowser.LoadCompleted -= Login_Method;
+                PixivBrowser.LoadCompleted -= CheckLogin;
+                cookie = GetCookieContainer();
+                MessageBox.Show("Success!");
+            }
+        }
+
+        public void ImagePageNumberTextChange(int n)
+        {
+            this.Dispatcher.BeginInvoke(DispatcherPriority.Normal, (ThreadStart)delegate()
+            {
+                SearchingNowTextPageNumber.Content = n.ToString() + "번째 페이지 탐방중";
+            });
+            this.Dispatcher.Invoke(
+   (ThreadStart)(() => { }), DispatcherPriority.ApplicationIdle);
+        }
+
+        public void ImageCountNumberTextChange(int n)
+        {
+            this.Dispatcher.BeginInvoke(DispatcherPriority.Normal, (ThreadStart)delegate()
+            {
+                    SearchingNowTextImageCount.Content = n.ToString() + "개 이미지 수집중";
+                  
+             });
+            this.Dispatcher.Invoke(
+       (ThreadStart)(() => { }), DispatcherPriority.ApplicationIdle);
+        }
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (!((Int32.Parse(MainThread_Number.Text)) > 0 && (Int32.Parse(MainThread_Number.Text)) <= 10))
+                {
+                    MessageBox.Show("효율은 1부터 10 사이의 숫자여야합니다.");
+                    return;
+                }
+            }
+            catch(Exception ex)
+            {
+                return;
+            }
+            Dispatcher.Invoke(DispatcherPriority.Normal, new Action(delegate{
+                SearchButton.IsEnabled = false;
+            }));
+            this.Dispatcher.Invoke(
+       (ThreadStart)(() => { }), DispatcherPriority.ApplicationIdle);
+            //ImageList ImageClass = new ImageList(PixivBrowser, TagTextBox.Text,cookie);
+            CollectImage ImageClass;
+            try
+            {
+                if (RadioButton1.IsChecked == true) ImageClass = new CollectImage(Int32.Parse(MainThread_Number.Text), this, Int32.Parse(StartPageTextBox.Text), Int32.Parse(EndPageTextBox.Text), Int32.Parse(BookMarkNumberTextBox.Text)
+                       , TagTextBox.Text, cookie);
+                else ImageClass = new CollectImage(Int32.Parse(MainThread_Number.Text),this, Int32.Parse(ImageNumber.Text), Int32.Parse(BookMarkNumberTextBox.Text), TagTextBox.Text, cookie);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        public void SearchButtonMakeEnable()
+        {
+            Dispatcher.Invoke(DispatcherPriority.Normal, new Action(delegate
+            {
+                SearchButton.IsEnabled = true;
+                SearchingNowTextPageNumber.Content = "";
+                SearchingNowTextImageCount.Content = "";
+                MessageBox.Show("Complete!");
+            }));
+            this.Dispatcher.Invoke(
+       (ThreadStart)(() => { }), DispatcherPriority.ApplicationIdle);
+        }
+
+        public void GetCookieLoad(object sender,NavigationEventArgs e)
+        {
+            cookie = GetCookieContainer();
+            PixivBrowser.LoadCompleted -= GetCookieLoad;
+            MessageBox.Show("Success!");
+        }
+
+        public CookieContainer GetCookieContainer()
+        {
+            CookieContainer container = new CookieContainer();
+
+            var Doc = PixivBrowser.Document as HTMLDocument;
+
+            foreach (string cookie in Doc.cookie.Split(';'))
+            {
+                string name = cookie.Split('=')[0];
+                string value = cookie.Substring(name.Length + 1);
+                string path = "/";
+                string domain;
+                if (name.Equals("login_ever") || name.Equals("GCSCU_89993436389_H3")) domain = ".www.pixiv.net"; //change to your domain name
+                else domain = ".pixiv.net"; //change to your domain name
+                container.Add(new Cookie(name.Trim(), value.Trim(), path, domain));
+            }
+
+            return container;
+        }
+    }
+}
